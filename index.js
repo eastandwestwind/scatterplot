454b8da91f274f1fa874ba016c769b0f3839fde8
catherineleesmith <ct-catherine.smith@build.com>
2018-01-13 19:53:21 -0500

    using dist from node modules for axios

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ app.get('/api/data', function (req, res) {
 });
 
 app.use(express.static('static'));
+app.use('/axios', express.static(__dirname + '/node_modules/axios/dist/'));
 
 app.listen(3000);
